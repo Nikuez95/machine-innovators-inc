6adddb6582a08cd5cc1251e73cf9ca80711e97a3
nikuez <nikuez@gmail.com>
2025-10-24 17:34:57 +0200

    added Unit Test and updated gitignore

---
+++ app/tests/test_main.py
@@ -0,0 +1,21 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "text/html; charset=utf-8"
+
+def test_sentiment_positive():
+    response = client.post("/analyze", json={"text": "I love programming!"})
+    assert response.status_code == 200
+    data = response.json()
+    assert data["sentiment_result"]['label'] == 'positive'
+
+def test_sentiment_negative():
+    response = client.post("/analyze", json={"text": "I hate bugs."})
+    assert response.status_code == 200
+    data = response.json()
+    assert data["sentiment_result"]['label'] == 'negative'
